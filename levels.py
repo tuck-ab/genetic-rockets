26fdf94803ff94724c7c1a958b5b3b6a1fcbb221
tuck-ab <alext.5@hotmail.co.uk>
2020-10-29 18:45:12 +0000

    Added the levels to a different file

---
+++ levels.py
@@ -0,0 +1,13 @@
+def shape_one():
+    obsticles = []
+    obsticles.append([200,500,300,10])
+    obsticles.append([0,300,300,10])
+    return obsticles
+
+def shape_two():
+    obsticles = []
+    obsticles.append([150,500,350,10])
+    obsticles.append([150,400,10,100])
+    obsticles.append([0,300,300,10])
+    obsticles.append([300,300,10,100])
+    return obsticles
